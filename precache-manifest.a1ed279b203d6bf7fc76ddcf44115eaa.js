4e70d53bca036e5eb1c4a7c99e9df615dca35b96
chiteshbansal <63744720+chiteshbansal@users.noreply.github.com>
2020-11-01 19:07:24 +0530

    Updates

---
+++ precache-manifest.a1ed279b203d6bf7fc76ddcf44115eaa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8024ff4e08f50fa3deef97e684a167fc",
+    "url": "/bookshelf/index.html"
+  },
+  {
+    "revision": "a5d747ccb3eb446740c7",
+    "url": "/bookshelf/static/css/main.36f5e564.chunk.css"
+  },
+  {
+    "revision": "83786e44032bbae6a781",
+    "url": "/bookshelf/static/js/2.9a2db9b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bookshelf/static/js/2.9a2db9b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5d747ccb3eb446740c7",
+    "url": "/bookshelf/static/js/main.20f6c011.chunk.js"
+  },
+  {
+    "revision": "91384a00a21254018d11",
+    "url": "/bookshelf/static/js/runtime-main.a728f37f.js"
+  },
+  {
+    "revision": "f0358ea4f46a717e76bc3ca34b28461d",
+    "url": "/bookshelf/static/media/image1.f0358ea4.jpg"
+  },
+  {
+    "revision": "346fcfd28cbad1fbbc92f272806a33f4",
+    "url": "/bookshelf/static/media/image2.346fcfd2.jpg"
+  },
+  {
+    "revision": "08168675b87f3c0b6e910feb0b1fa274",
+    "url": "/bookshelf/static/media/loginform2.08168675.jpg"
+  },
+  {
+    "revision": "9f6d95778d8990b67963e9416f81b474",
+    "url": "/bookshelf/static/media/userProfileSideBar.9f6d9577.jpg"
+  }
+]);
